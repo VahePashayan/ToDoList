31b59b0aa8d63b212f5fb5b8171150815bad926c
Vahe <you@example.com>
2021-07-24 21:30:03 +0400

    refactor: relocated add button from ToDo.jsx to Input.jsx in Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const Input = ({value, onChange}) => {
+const Input = ({value, onChange, onClick}) => {
     return <span>
         <input type="text" placeholder="you have to do..." value={value} onChange={onChange} autoFocus />
+        <button onClick={onClick}>Add</button>
     </span>;
 }
  
--- src/components/ToDo/ToDo.jsx
+++ src/components/ToDo/ToDo.jsx
@@ -1,43 +1,44 @@
-import React, { Component } from 'react'
-import Input from '../Input'
-import Task from '../Task'
-import './ToDo.css'
-
-
-
+import React, { Component } from "react";
+import Input from "../Input";
+import Task from "../Task";
+import "./ToDo.css";
 
 class ToDo extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = { 
-            inputValue: "",
-            tasksList: []
-         }
-    }
-    inputList = [];
+    this.state = {
+      inputValue: "",
+      tasksList: [],
+    };
+  }
+  inputList = [];
 
-    handleAdd = () => {
-        this.inputList.push(this.state.inputValue)
-        this.setState({tasksList: this.inputList});
-        this.setState({inputValue: ""});
-        console.log(this.inputList);
-    } 
+  handleAdd = () => {
+    this.inputList.push(this.state.inputValue);
+    this.setState({ tasksList: this.inputList });
+    this.setState({ inputValue: "" });
+    console.log(this.inputList);
+  };
 
-    handleChange = (e) => {
-        this.setState({inputValue: e.target.value})
-    }
+  handleChange = (e) => {
+    this.setState({ inputValue: e.target.value });
+  };
 
-    render() { 
-
-        return ( <div className="container">
-            <span>
-                <Input value={this.state.inputValue} onChange={this.handleChange}/>
-                <button onClick={this.handleAdd}>Add</button>
-            </span>
-            <Task list={this.state.tasksList}/>
-    </div> );
-    }
+  render() {
+    return (
+      <div className="container">
+        <span>
+          <Input
+            value={this.state.inputValue}
+            onChange={this.handleChange}
+            onClick={this.handleAdd}
+          />
+        </span>
+        <Task list={this.state.tasksList} />
+      </div>
+    );
+  }
 }
- 
+
 export default ToDo;
